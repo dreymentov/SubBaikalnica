93a049675d179963e03684ce162e88afab6f2a1b
Dmitriy Storozhuk <dreymentov@mail.ru>
2023-12-06 11:50:53 +0300

    v0.001 pre-alpha

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Init : MonoBehaviour
+{
+    public PlayerData PlayerData;
+
+    public static Init Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+
+        if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+[System.Serializable]
+public class PlayerData
+{
+    public int PlayerLevel = 1;
+    public int PlayerExperience = 1;
+    public int PlayerMoney = 100;
+    public int PlayerHardMoney = 10;
+}
